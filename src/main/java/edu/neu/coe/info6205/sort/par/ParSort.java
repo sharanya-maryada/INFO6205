5526b9f1aee2a774c74b6df2e0fdbe5a0471c353
sharanya-maryada <98719776+sharanya-maryada@users.noreply.github.com>
2023-02-18 21:24:06 -0500

    Update ParSort

--- src/main/java/edu/neu/coe/info6205/sort/par/ParSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/ParSort.java
@@ -2,24 +2,23 @@ package edu.neu.coe.info6205.sort.par;
 
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * This code has been fleshed out by Ziyao Qiao. Thanks very much.
- * CONSIDER tidy it up a bit.
+ * TODO tidy it up a bit.
  */
 class ParSort {
 
     public static int cutoff = 1000;
 
-    public static void sort(int[] array, int from, int to) {
+    public static void sort(int[] array, int from, int to, ForkJoinPool threadPool) {
         if (to - from < cutoff) Arrays.sort(array, from, to);
         else {
-            // FIXME next few lines should be removed from public repo.
-            CompletableFuture<int[]> parsort1 = parsort(array, from, from + (to - from) / 2); // TO IMPLEMENT
-            CompletableFuture<int[]> parsort2 = parsort(array, from + (to - from) / 2, to); // TO IMPLEMENT
-            CompletableFuture<int[]> parsort = parsort1.thenCombine(parsort2, (xs1, xs2) -> {
+            CompletableFuture<int[]> parallelSort1Fut = parsort(array, from, from + (to - from) / 2, threadPool);
+            CompletableFuture<int[]> parallelSort2Fut = parsort(array, from + (to - from) / 2, to, threadPool);
+            CompletableFuture<int[]> parallelSortFut = parallelSort1Fut.thenCombine(parallelSort2Fut, (xs1, xs2) -> {
                 int[] result = new int[xs1.length + xs2.length];
-                // TO IMPLEMENT
                 int i = 0;
                 int j = 0;
                 for (int k = 0; k < result.length; k++) {
@@ -36,21 +35,19 @@ class ParSort {
                 return result;
             });
 
-            parsort.whenComplete((result, throwable) -> System.arraycopy(result, 0, array, from, result.length));
-//            System.out.println("# threads: "+ ForkJoinPool.commonPool().getRunningThreadCount());
-            parsort.join();
+            parallelSortFut.whenComplete((result, throwable) -> System.arraycopy(result, 0, array, from, result.length));
+            parallelSortFut.join();
         }
     }
 
-    private static CompletableFuture<int[]> parsort(int[] array, int from, int to) {
+    private static CompletableFuture<int[]> parsort(int[] array, int from, int to, ForkJoinPool threadPool) {
         return CompletableFuture.supplyAsync(
                 () -> {
                     int[] result = new int[to - from];
-                    // TO IMPLEMENT
                     System.arraycopy(array, from, result, 0, result.length);
-                    sort(result, 0, to - from);
+                    sort(result, 0, to - from, threadPool);
                     return result;
-                }
+                }, threadPool
         );
     }
 }
